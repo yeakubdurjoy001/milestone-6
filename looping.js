397252bb18baf7c5e59d42603ed10e1047865b42
YeakubDurjoy001 <your.email@example.com>
2024-03-08 18:25:21 +0600

    m-31

---
+++ looping.js
@@ -0,0 +1,19 @@
+const numbers = [1, 5, 35, 8, 9, 5, 2, 97];
+for (const num of numbers) {
+  //   console.log(num);
+}
+const nobab = "siraj ud doula";
+for (const char of nobab) {
+  //   console.log(char);
+}
+const glass = {
+  name: "glass",
+  color: "golden",
+  price: 200,
+  weight: 1,
+  height: 100,
+  density: 400,
+};
+for (const key in glass) {
+  console.log(key);
+}
